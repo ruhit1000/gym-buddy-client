9de899e5ecd17f57da84eb0cba004fc28d05e599
Ruhit Paul <ruhitpaul18@gmail.com>
2026-06-22 13:43:44 +0600

    feat: add authHeader function for token management in server requests

--- src/lib/core/server.js
+++ src/lib/core/server.js
@@ -1,10 +1,19 @@
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
+export const authHeader = async () => {
+  const token = await getUserToken();
+  const header = token ? {
+    authorization: `Bearer ${token}`,
+  } : {};
+  return header;
+}
+
 export const serverMutation = async (path, method, data) => {
   const res = await fetch(`${baseUrl}/api/${path}`, {
     method: method,
     headers: {
       "Content-Type": "application/json",
+      ...await authHeader(),
     },
     body: JSON.stringify(data),
   });
